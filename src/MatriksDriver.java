6d52c0f9ebf71fa2539b6a092f073f04b58169e4
Akifa Nabil <68159046+akifanabil@users.noreply.github.com>
2020-09-29 17:49:49 +0700

    input driver make GUI

--- src/MatriksDriver.java
+++ src/MatriksDriver.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class MatriksDriver {
 
     static int pil1;
     static int pil2;
     static int pil3;
     static int pil4;
+    static int pilinput;
 
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
@@ -14,51 +17,87 @@ public class MatriksDriver {
 
         MainMenu();
         while (pil1!=6){
+            jenisInput();
+
+            //Memperoleh input berdasar pilihan metode input yang dipilih
+            if (pilinput==1){
+                if (pil1==1){
+                    M.BacaMatriks();
+                } else if (pil1==2 || pil1==3){
+                    M.BacaMatriksPersegi();
+                }
+            } else{
+                // Input dibaca dari file
+                M.BacaFileMatriks("matriks.txt");
+            }
+
+    
+            // Menjalankan menu pilihan
             if (pil1==1){
                 // Penyelesaian SPL
+
+                //Menampilkan menu pilihan metode penyelesaian SPL
                 Menu1();
-                System.out.println("1");
+
+                //Melakukan pemrosesan untuk mencari penyelesaian SPL sesuai input pilihan metode penyelesaian SPL
+                if (pil2==1){
+
+                } else if(pil2==2){
+
+                } else if (pil2==3){
+
+                } else{
+
+                }
+
             } else if (pil1==2){
-                // Determinan
+                // Mencari Determinan
+
+                //Menampilkan menu pilihan metode pencarian nilai determinan
                 Menu2();
                 if (pil3==1) {
-                    M.BacaMatriksPersegi();
-                    System.out.println("Determinan dengan Metode Reduksi Baris : " + Determinan1(M));
-                }
-                else if (pil3==2) {
-                    System.out.println("Determinan 2"); //Belum
+                    System.out.println("Determinan dengan Metode Reduksi Baris : " + M.Determinan1(M));
                 }
                 else {
-                    System.out.println("Input salah. Harap masukan kembali input sesuai nomor menu pilihan.");
+                    System.out.println("Determinan 2"); //Belum
                 }
+
             } else if (pil1==3){
                 // Mencari Invers
+
+                // Menampilkan pilihan menu metode pencarian invers
                 Menu3();
+
+                //Melakukan proses pencarian invers matriks berdasarkan pilihan metode
                 if (pil4==1){
                     // Dengan menggunakan metode Gauss Jordan
-                    M.BacaMatriksPersegi();
-                    M.InversMatriks1(); 
+                    M.InversMatriks1();
                 } else if (pil4==2){
-                    // Dengan menggunakan perkalian 1/determinan dan Adjoin matriks
-                    M.BacaMatriksPersegi();
                     M.InversMatriks2();
-                } else{
-                    System.out.print("Input salah. Harap masukan kembali input sesuai nomor menu pilihan.");
-                    Menu3();
                 }
+
             } else if (pil1==4){
                 //Interpolasi polinom
-                System.out.println("4");
-            } else if (pil1==5){
+                System.out.println(4);
+
+            } else{
                 //Regresi linear berganda
                 M.regresilinearganda();
-            } else{
-                System.out.println("Harap masukkan pilihan sesuai nomor menu pilihan");
             }
             MainMenu();
         }
-        System.out.println("--------------------------------------------------------------------------------");
-        System.out.println("Terimakasih");
+        
+        System.out.println("\n--------------------------------------------------------------------------------");
+        System.out.println("Terimakasih!");
+        System.exit(0);
+    }
+
+    static void validateinput(int input,int min,int max){
+        String sinput;
+        while (input<min || input>max){
+            sinput = JOptionPane.showInputDialog("Invalid Input. Masukkan angka pilihan menu ("+min+"-"+max+") : ");
+            input = Integer.parseInt(sinput);
+        }
     }
 
     public static void MainMenu(){
@@ -73,38 +112,51 @@ public class MatriksDriver {
         System.out.println("4. Interpolasi Polinom");
         System.out.println("5. Regresi Linear Berganda");
         System.out.println("6. Keluar");
-        System.out.print("\nMasukkan nomor pilihan menu : ");
-        pil1 = scanner.nextInt();
+        String spil1 = JOptionPane.showInputDialog("Pilihan Menu (1-6) : ");
+        pil1 = Integer.parseInt(spil1);
+        validateinput(pil1, 1, 6);
     }
 
     public static void Menu1(){
         //method menampilkan list menu spl
-        System.out.println("Pilihan metode peyelesaian SPL : ");
+        System.out.println("\nPilihan metode peyelesaian SPL : ");
         System.out.println("1. Metode Eliminasi Gauss");
         System.out.println("2. Metode Eliminasi Gauss-Jordan");
         System.out.println("3. Metode Matriks Balikan");
         System.out.println("4. Kaidah Cramer");
-        System.out.print("\nMasukkan nomor pilihan menu : ");
-        pil2 = scanner.nextInt();
+        String spil2 = JOptionPane.showInputDialog("Pilihan Menu (1-4) : ");
+        pil2 = Integer.parseInt(spil2);
+        validateinput(pil2,1,4);
     }
 
     public static void Menu2(){
         //method menampilkan list determinan
-        System.out.println("Pilihan metode pencarian Determinan : ");
+        System.out.println("\nPilihan metode pencarian Determinan : ");
         System.out.println("1. Metode Reduksi Baris");
         System.out.println("2. Metode Ekspansi Kofaktor");
-        System.out.print("\nMasukkan nomor pilihan menu : ");
-        pil3 = scanner.nextInt();
+        String spil3 = JOptionPane.showInputDialog("Pilihan Menu (1-2) : ");
+        pil3 = Integer.parseInt(spil3);
+        validateinput(pil3,1,2);
+
     }
 
     public static void Menu3(){
         // method menampilkan list invers
-        System.out.println("Pilihan metode pencarian Invers Matriks : ");
+        System.out.println("\nPilihan metode pencarian Invers Matriks : ");
         System.out.println("1. Metode Reduksi Baris");
         System.out.println("2. Menggunakan Determinan dan Adjoin");
-        System.out.print("\nMasukkan nomor pilihan menu : ");
-        pil4 = scanner.nextInt();
+        String spil4 = JOptionPane.showInputDialog("Pilihan Menu (1-2) : ");
+        pil4 = Integer.parseInt(spil4);
+        validateinput(pil4,1,2);
     }
 
-
+    public static void jenisInput(){
+        // menampilkan list pilihan metode input (keyboard/file)
+        System.out.println("\nPilihan metode input data matriks : ");
+        System.out.println("1. Keyboard");
+        System.out.println("2. File txt");
+        String spilinput = JOptionPane.showInputDialog("Pilihan Menu (1-2) : ");
+        pilinput= Integer.parseInt(spilinput);
+        validateinput(pilinput,1,2);
+    }
 }
